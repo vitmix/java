f830ff214b4d17dc11558908af95dcef7b6b7392
vitmix <47299766+vitmix@users.noreply.github.com>
2020-04-27 12:55:01 +0300

    Add isAlive() calls

--- labs/src/RunAll/ParallelMain.java
+++ labs/src/RunAll/ParallelMain.java
@@ -13,11 +13,15 @@ public class ParallelMain {
 
             // running rows sorter lab
             FirstLabExecutor t1 = new FirstLabExecutor("Rows Sorting Executor", reader);
+            System.out.println(t1.getThread().getName() + " is alive ? " + t1.getThread().isAlive());
             t1.getThread().join();
+            System.out.println(t1.getThread().getName() + " is alive ? " + t1.getThread().isAlive());
 
             // running ip validator lab
             SecondLabExecutor t2 = new SecondLabExecutor("Ip Validation Executor", reader);
+            System.out.println(t2.getThread().getName() + " is alive ? " + t2.getThread().isAlive());
             t2.getThread().join();
+            System.out.println(t2.getThread().getName() + " is alive ? " + t2.getThread().isAlive());
 
         } catch (InterruptedException e) {
             System.out.println("Main thread was interrupted.");
